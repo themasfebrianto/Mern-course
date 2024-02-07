e2b21dc99b1eafc70cbec365a815521f4223e356
themas <themasfebrianto13@gmail.com>
2024-02-07 15:50:48 +0700

    add a middleware and function to handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,14 +1,13 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     try {
         const { userName, email, password } = req.body;
 
         if (!userName || !email || !password || userName === "" || email === "" || password === "") {
-            return res.status(400).json({
-                message: 'All Field are required'
-            });
+            next(errorHandler(400, 'All fields are required'))
         }
 
         const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -22,8 +21,6 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.json("SignUp Successful");
     } catch (error) {
-        return res.status(500).json({
-            message: error.message
-        });
+        next(error);
     }
 }
--- api/index.js
+++ api/index.js
@@ -22,6 +22,17 @@ app.use(express.json());
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'internal server error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+});
+
 app.listen(3000, () => {
     console.log("server is running on port 3000")
 });
