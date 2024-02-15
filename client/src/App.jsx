dfe7160a87f98636594245878365c747180b6abc
themas <themasfebrianto13@gmail.com>
2024-02-15 11:17:49 +0700

    create and add the footer components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Dashboard from './pages/Dashboard'
@@ -7,19 +7,21 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Projects from './pages/Projects'
 import Header from './components/Header'
+import FooterCom from './components/Footer'
 
 export default function App() {
   return (
-   <BrowserRouter>
-   <Header/>
-    <Routes>
-       <Route path='/' element={<Home/>}/>
-       <Route path='/about' element={<About/>}/>
-       <Route path='/dashboard' element={<Dashboard/>}/>
-       <Route path='/sign-in' element={<SignIn/>}/>
-       <Route path='/sign-up' element={<SignUp/>}/>
-       <Route path='/projects' element={<Projects/>}/>
-    </Routes>
-   </BrowserRouter>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+      <FooterCom />
+    </BrowserRouter>
   )
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -45,7 +45,7 @@ export default function SignUp() {
         {/* Left Side */}
         <div className='flex-1'>
           <Link to="/" className=' font-bold dark:text-white text-4xl'>
-            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Tem's</span>
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Themas</span>
             Blog
           </Link>
           <p className='text-sm mt-5'>
